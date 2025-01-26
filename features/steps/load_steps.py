0f5311a6b082d1c1337b9697e4126d16f1018f29
Zeni04038 <19bt04038@gsfcuniversity.ac.in>
2025-01-26 13:24:42 -0500

    Implemented Steps for matching

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -42,10 +42,7 @@ def step_impl(context):
     for product in context.resp.json():
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
         assert(context.resp.status_code == HTTP_204_NO_CONTENT)
-
-    #
     # load the database with new products
-    #
     for row in context.table:
         # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
         payload = {
